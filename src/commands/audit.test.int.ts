f84c88bc8fc21765f14c11e5190544865c116798
Jack Schofield <jjmschofield@googlemail.com>
2020-07-21 03:48:23 +0100

    Preventing. fixture from failing build

--- src/commands/audit.test.int.ts
+++ src/commands/audit.test.int.ts
@@ -1,5 +1,5 @@
 import { v4 as uuidv4 } from 'uuid';
-import { generateProject } from './__tests__/project.test.fixture';
+import { generateProject } from './__fixtures__/project.fixture';
 
 import util from 'util';
 
