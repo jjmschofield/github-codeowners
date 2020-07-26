458defc906b26275ef337a1ca440aef5015dff9a
Jack Schofield <jjmschofield@googlemail.com>
2020-07-26 04:16:56 +0100

    providing a replacement to recursive-readdir which doesn't fail on symlinks

--- src/lib/dir.ts
+++ src/lib/dir.ts
@@ -1,19 +1,48 @@
-import readdir from'recursive-readdir';
-import { createGitIgnoreFilter } from './gitignore';
+import fs, { Stats } from 'fs';
+import ignore, { Ignore } from 'ignore';
+import path from 'path';
 
-export const getFilteredFilePaths = async (dir: string, root: string): Promise<string[]> => {
-  const filePaths = await readdir(dir, ['.git']);
 
-  const trimmedFilePaths = filePaths.map((filePath: string) => {
-    return filePath.replace(`${dir}/`, '');
+export const readDirRecursively = async (dir: string, filters: string[] = []): Promise<string[]> => {
+  return new Promise((resolve, reject) => {
+    try {
+      const ignores = ignore().add(filters);
+      const files = walkDir(dir, ignores);
+      resolve(files);
+    } catch (e) {
+      reject(e);
+    }
   });
-
-  const filteredFilePaths = trimmedFilePaths
-    .filter((file: string) => file.startsWith(root))
-    .filter(await createGitIgnoreFilter(dir));
-
-  filteredFilePaths.sort();
-
-  return filteredFilePaths;
 };
 
+const walkDir = (dir: string, ignores: Ignore, files: string[] = []): string[] => {
+  if (!fs.existsSync(dir)) return files;
+
+  const newFiles = fs.readdirSync(dir);
+
+  for (const file of newFiles) {
+    if (ignores.ignores(file)) {
+      continue;
+    }
+
+    const fullPath = path.join(dir, file);
+
+    let stats: Stats | undefined = undefined;
+
+    try {
+      stats = fs.statSync(fullPath);
+    } catch (e) {
+      continue;// Ignore missing files and symlinks
+    }
+
+    if (stats && stats.isDirectory()) {
+      walkDir(fullPath, ignores, files);
+    }
+
+    if (stats && stats.isFile()) {
+      files.push(fullPath);
+    }
+  }
+
+  return files;
+};
