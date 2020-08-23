d506414997b7128773684a4ecb52cbaa462f01f4
Jack Schofield <jjmschofield@googlemail.com>
2020-08-23 13:19:34 +0100

    switching to using exec for windows suppoort

--- src/lib/ownership/lib/readTrackedGitFiles.ts
+++ src/lib/ownership/lib/readTrackedGitFiles.ts
@@ -1,6 +1,6 @@
-import { execFile } from '../../util/execFile';
+import { exec } from '../../util/exec';
 
 export const readTrackedGitFiles = async (dir: string): Promise<string[]> => {
-  const { stdout } = await execFile('git', ['ls-files'], { cwd: dir });
+  const { stdout } = await exec('git ls-files', { cwd: dir });
   return stdout.split('\n').filter(x => !!x);
 };
