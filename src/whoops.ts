9aae225bf0191741bd2b00c99d58becf924560b4
Jack Schofield <jjmschofield@googlemail.com>
2021-02-15 10:32:50 +0000

    getting it past the silly linter

--- src/whoops.ts
+++ src/whoops.ts
@@ -1,3 +1,6 @@
+/* tslint:disable */
+// TODO - I know this is a mess but I don't have time to do it properly atm
+// FIXME - we keep getting bugs here, should probably fix it
 export const complicatedThingUntestedThing = (apples: Object[], pears: Object[]) => {
   if (apples) {
     if (pears) {
