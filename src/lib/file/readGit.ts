df254e36b2f87b6be00eee1e2e0f05ecdae5b784
T0RAT0RA <stephane.reiss@gmail.com>
2022-01-25 05:51:16 -0500

    Exclude non file paths to avoid reading a directory (#64)

--- src/lib/file/readGit.ts
+++ src/lib/file/readGit.ts
@@ -1,6 +1,21 @@
+import fs, { Stats } from 'fs';
 import { exec } from '../util/exec';
 
 export const readGit = async (dir: string): Promise<string[]> => {
   const { stdout } = await exec('git ls-files', { cwd: dir });
-  return stdout.split('\n').filter(x => !!x);
+  return stdout.split('\n').filter((filePath) => {
+    let stats: Stats | undefined = undefined;
+    try {
+      stats = fs.statSync(filePath);
+    } catch (e) {
+      return false; // Ignore missing files and symlinks
+    }
+
+    // Ignore if path is not a file
+    if (!stats.isFile()){
+      return false;
+    }
+
+    return true;
+  });
 };
