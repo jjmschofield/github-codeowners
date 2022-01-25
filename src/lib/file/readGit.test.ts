04e6510ce21136dbd8fb97540c3b5ba9c120aa16
Stéphane Reiss <stephane.reiss@gmail.com>
2022-01-24 21:09:47 -0500

    Exclude non file paths to avoid reading a directory

--- src/lib/file/readGit.test.ts
+++ src/lib/file/readGit.test.ts
@@ -1,10 +1,24 @@
 import { exec } from '../util/exec';
 import { readGit } from './readGit';
+import fs from 'fs';
 
 jest.mock('../util/exec');
+jest.mock('fs');
+const fsMocked = jest.mocked<any>(fs);
 const execFileMock = exec as jest.Mock;
 
 describe('readGit', () => {
+  beforeEach(() => {
+    fsMocked.statSync.mockImplementation((path: any) => {
+      return {
+        isFile() {
+          if (!path) { return false; }
+          return true;
+        },
+      };
+    });
+  });
+
   it('should return the expected list of files when called', async () => {
     execFileMock.mockResolvedValue({ stdout: 'foo\nbar\n', stderr: '' });
 
@@ -24,5 +38,21 @@ describe('readGit', () => {
         cwd: 'some/dir',
       }),
     );
+  });
+
+  it('should not return non-files', async () => {
+    execFileMock.mockResolvedValue({ stdout: 'foo\nbar\nbaz\n', stderr: '' });
+    fsMocked.statSync.mockImplementation((path: any) => {
+      return {
+        isFile() {
+          if (!path || path === 'baz') { return false; }
+          return true;
+        },
+      };
+    });
+
+    const result = await readGit('some/dir');
+
+    expect(result).toStrictEqual(['foo', 'bar']);
   });
 });
