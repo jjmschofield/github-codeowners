6cdcf296a71b5d0516d632e1955b0c3663d3d3dd
Luke Albao <luke.albao@webflow.com>
2024-01-25 14:49:59 -0800

    Extend git ls-files maxBuffer to 3MiB

--- src/lib/file/readGit.ts
+++ src/lib/file/readGit.ts
@@ -2,7 +2,7 @@ import fs, { Stats } from 'fs';
 import { exec } from '../util/exec';
 
 export const readGit = async (dir: string): Promise<string[]> => {
-  const { stdout } = await exec('git ls-files', { cwd: dir });
+  const { stdout } = await exec('git ls-files', { cwd: dir, maxBuffer: 3 * 2 ** 20, }); // 3MiB
   return stdout.split('\n').filter((filePath) => {
     let stats: Stats | undefined = undefined;
     try {
